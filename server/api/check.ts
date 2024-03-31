f5fea713ab9c34846facf17b40f5a6657f83b7aa
Alois Sečkár <ellrohir@seznam.cz>
2024-03-31 22:01:39 +0200

    feat: scrap client-rendered content using Puppeteer

--- server/api/check.ts
+++ server/api/check.ts
@@ -1,35 +1,49 @@
 import { parse } from 'node-html-parser'
+import { launch } from 'puppeteer'
 import type { PBPCheck } from '../utils/types'
 
 export default defineEventHandler(async (): Promise<PBPCheck> => {
-  // GET content from page
-  const jepHTMLData = await $fetch<string>('https://stats.baseball.cz/cs/events/2023-extraliga/schedule-and-results/box-score/116387')
+  const browser = await launch()
 
-  const jepPage = parse(jepHTMLData)
-  console.log(jepPage)
+  let pbpHTMLData = ''
+  try {
+    // the page is client-generated => it requires headless browser to render it
 
-  /*
-  // entries are listed in two <table class="jeps"> elements
-  const tables = jepPage.querySelectorAll('.jeps')
-  // first table containts the actual JEPs
-  const jepTable = tables[0]
-  // second table lists drafts yet to be promoted to JEP (or left forsaken)
-  const draftTable = tables[1]
+    // use Puppeteer to navigate to page
+    const page = await browser.newPage()
+    await page.goto('https://stats.baseball.cz/cs/events/2023-extraliga/schedule-and-results/box-score/116387')
+    // move to "PLAYS" tab (default is "BOX")
+    const playsLinks = await page.$$("xpath=//a[contains(text(), 'ROZEHRY')]")
+    await playsLinks[0].evaluate((link) => {
+      if (link instanceof HTMLElement) {
+        link.click()
+      }
+    })
+    // scrap the website as plain HTML string
+    pbpHTMLData = await page.content()
 
-  // now we seek last <tr> of each table - here will be the latest entry
-  const lastJEPRow = jepTable?.querySelectorAll('tr')?.at(-1)
-  const lastDraftRow = draftTable?.querySelectorAll('tr')?.at(-1)
+    // feed the scrapped data to 'node-html-parser' as it handles virtual DOM better
+    // TODO refactor this to use Puppeteer only?
+    const pbpPage = parse(pbpHTMLData)
 
-  // the target number resides inside (the only one) <td class="jep">
-  // element nested inside <tr>'s chlidren
-  const lastJEPCell = lastJEPRow?.querySelector('.jep')
-  const lastDraftCell = lastDraftRow?.querySelector('.jep')
+    const plays = pbpPage.querySelectorAll('.plays-row')
+    plays.forEach((play) => {
+      if (play.innerHTML.includes('GAME OVER')) {
+        // wrap-up
+        pbpHTMLData = play.innerHTML
+        // TODO check if W/L/S is filled
+        // TODO check if W/L/S is correct
+      }
+      if (play.innerHTML.includes('singles') || play.innerHTML.includes('doubles') || play.innerHTML.includes('triples')) {
+        // hit
+        // TODO check if there is no forced out (cannot be hit)
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
 
-  // finally the value is a sole TextNode inside <td> we just grabbed
-  // and we can access it directly via .text attribute
-  const lastJEPNo = lastJEPCell?.text
-  const lastDraftNo = lastDraftCell?.text
-  */
+  browser.close()
 
   // return JSON data
   return {
@@ -38,7 +52,7 @@ export default defineEventHandler(async (): Promise<PBPCheck> => {
     games: {
       game: '2023-03-31 - #2 - ARR vs. HLU',
       result: 'OK',
-      problems: []
+      problems: [pbpHTMLData]
     }
   }
 })
