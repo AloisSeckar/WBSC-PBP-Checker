5ca7eda7b4c9a404df8a480a9268d748832b2d83
Alois Sečkár <ellrohir@seznam.cz>
2024-04-22 21:02:28 +0200

    chore: temporery surpressing warning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  // TODO remove later https://github.com/nuxt-modules/tailwindcss/releases/tag/v6.12.0
+  tailwindcss: {
+    quiet: true
+  }
 })
