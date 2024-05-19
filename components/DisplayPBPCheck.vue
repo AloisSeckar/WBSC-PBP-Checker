d9ba5485dccb51760378c8482e89817fc7b650c7
Alois Sečkár <ellrohir@seznam.cz>
2024-05-19 15:53:04 +0200

    feat: filter for error-only results

--- components/DisplayPBPCheck.vue
+++ components/DisplayPBPCheck.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="my-4">
     <h2 class="mb-4 text-2xl font-bold">
-      Check result
+      Check results
     </h2>
     <div v-if="checkData.date" :class="cssClass">
-      <div><strong>Checked at:</strong> {{ checkData.date }}</div>
-      <div v-for="gameData in checkData.games" :key="gameData.link">
+      <div>
+        <strong>Display:</strong> All results <UToggle v-model="errorsOnly" /> Errors only
+      </div>
+      <div>
+        <strong>Checked at:</strong> {{ checkData.date }}
+      </div>
+      <div v-for="gameData in displayedData" :key="gameData.link">
         <DisplayPBPGameCheck :game-data="gameData" />
       </div>
     </div>
@@ -19,9 +24,20 @@ const props = defineProps({
   checkData: { type: Object as PropType<PBPCheck>, required: true },
 })
 
+const displayedData = computed(() => {
+  const games = props.checkData.games
+  if (errorsOnly.value) {
+    return games.filter(g => g.result !== 'OK')
+  } else {
+    return games
+  }
+})
+
 const cssClass = computed(() => {
   const baseCss = 'px-4 py-2 rounded-lg border-2 border-black text-gray-900'
   const colorsCss = props.checkData.result === 'OK' ? 'bg-primary-100' : 'bg-red-100'
   return baseCss + ' ' + colorsCss
 })
+
+const errorsOnly = ref(false)
 </script>
