286ee79303a01b044d0e94f7375bedfa4899407a
Alois Sečkár <ellrohir@seznam.cz>
2024-07-04 19:31:29 +0200

    build: tech update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-07-04',
+
   devtools: {
     enabled: false,
   },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
     '@vueuse/nuxt',
     'nuxt-cron',
   ],
+
   eslint: {
     config: {
       stylistic: true,
     },
   },
+
   // TODO remove later https://github.com/nuxt-modules/tailwindcss/releases/tag/v6.12.0
   tailwindcss: {
     quiet: true,
   },
+
   runtimeConfig: {
     emailPassword: '',
     public: {
