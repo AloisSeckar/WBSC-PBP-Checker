0a1b00882805b9668febc0a960affdb6eb63c6d8
Alois Sečkár <ellrohir@seznam.cz>
2024-07-25 20:55:10 +0200

    chore: CSS can be minified again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,13 +24,6 @@ export default defineNuxtConfig({
     quiet: true,
   },
 
-  // TODO this temporary for investigating behavior on Netlify
-  vite: {
-    build: {
-      minify: false,
-    },
-  },
-
   runtimeConfig: {
     emailPassword: '',
     public: {
