b550fcad7ea52972db4d2325605873af3b1bc94c
Alois Sečkár <ellrohir@seznam.cz>
2026-03-01 21:06:09 +0100

    fix: avoid memory leak from orphaned chromium instances

--- server/api/check.ts
+++ server/api/check.ts
@@ -2,147 +2,155 @@ import chromium from '@sparticuz/chromium'
 import puppeteer from 'puppeteer-core'
 
 export default defineEventHandler(async (event): Promise<PBPCheck> => {
+  const games: PBPGameCheck[] = []
+
   const body = await readBody(event) as PBPGameCheckRequest
   const gameLinks = body?.gameLinks
 
   const executablePath = import.meta.dev ? useRuntimeConfig().public.chromium as string : await chromium.executablePath()
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath,
-    headless: true,
-  })
-  const gamePage = await browser.newPage()
 
-  const games: PBPGameCheck[] = []
+  let browser
+  try {
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath,
+      headless: true,
+    })
+    const gamePage = await browser.newPage()
 
-  for (const gameLink of gameLinks) {
-    if (!gameLink || gameLink.trim() === '') {
+    for (const gameLink of gameLinks) {
+      if (!gameLink || gameLink.trim() === '') {
       // skip accidental empty lines
-      continue
-    }
-
-    const problems: string[] = []
-
-    let gameTitle = 'UNKNOWN'
-    let gameScorer = 'UNKNOWN'
-    let gameData
-    let appData
-    let gamePlays: WBSCGamePlays
-    try {
-      await gamePage.goto(gameLink)
-      await gamePage.waitForSelector('#app')
-      const rawData = await gamePage.$eval('#app', el => el.getAttribute('data-page'))
-      appData = JSON.parse(rawData).props.viewData.original as WBSCAppData
-
-      // skip scheduled, but non-played games
-      if (appData.gameData?.gamestatus === 0) {
-        const gameData = appData.gameData
-        games.push({
-          link: gameLink,
-          game: `#${gameData.gamenumber} - ${gameData.homeioc} vs. ${gameData.awayioc} (${gameData.start}) (NOT PLAYED)`,
-          scorer: 'Not assigned',
-          result: 'OK',
-          problems,
-        })
         continue
       }
 
-      // analyze input data
-      const inputProblems = analyzeInput(appData)
-      if (inputProblems.length > 0) {
-        problems.push(...inputProblems)
-      }
+      const problems: string[] = []
 
-      if (appData && problems.length == 0) {
-        let innings = 0
-        let homeTeamId = 0
-        let awayTeamId = 0
+      let gameTitle = 'UNKNOWN'
+      let gameScorer = 'UNKNOWN'
+      let gameData
+      let appData
+      let gamePlays: WBSCGamePlays
+      try {
+        await gamePage.goto(gameLink)
+        await gamePage.waitForSelector('#app')
+        const rawData = await gamePage.$eval('#app', el => el.getAttribute('data-page'))
+        appData = JSON.parse(rawData).props.viewData.original as WBSCAppData
 
-        let variant: PBPVariant = 'baseball'
-        let winner: PBPWinner = 'home'
-
-        variant = appData.tournamentInfo.innings === 9 ? 'baseball' : 'softball'
-
-        gameData = appData.gameData
-        homeTeamId = gameData.homeid
-        awayTeamId = gameData.awayid
-
-        innings = gameData.innings
-
-        // elaborate winning team
-        const homePoints = gameData.homeruns
-        const awayPoints = gameData.awayruns
-        if (homePoints === awayPoints) {
-          problems.push(`Game ended with a tie (${homePoints}:${awayPoints})`)
+        // skip scheduled, but non-played games
+        if (appData.gameData?.gamestatus === 0) {
+          const gameData = appData.gameData
+          games.push({
+            link: gameLink,
+            game: `#${gameData.gamenumber} - ${gameData.homeioc} vs. ${gameData.awayioc} (${gameData.start}) (NOT PLAYED)`,
+            scorer: 'Not assigned',
+            result: 'OK',
+            problems,
+          })
+          continue
         }
-        winner = homePoints > awayPoints ? 'home' : 'away'
 
-        // build game title
-        gameTitle = `#${gameData.gamenumber} - ${gameData.homeioc} ${homePoints} vs. ${awayPoints} ${gameData.awayioc} (${gameData.start})`
+        // analyze input data
+        const inputProblems = analyzeInput(appData)
+        if (inputProblems.length > 0) {
+          problems.push(...inputProblems)
+        }
 
-        // get scorer(s) name
-        const scorers: string[] = []
-        gameData.assignments.forEach((a) => {
-          if (a.type === 1) { // 0 = Umpires, 1 = scorers, 2 = TCs
-            scorers.push(a.person?.label || 'UNKNOWN')
+        if (appData && problems.length == 0) {
+          let innings = 0
+          let homeTeamId = 0
+          let awayTeamId = 0
+
+          let variant: PBPVariant = 'baseball'
+          let winner: PBPWinner = 'home'
+
+          variant = appData.tournamentInfo.innings === 9 ? 'baseball' : 'softball'
+
+          gameData = appData.gameData
+          homeTeamId = gameData.homeid
+          awayTeamId = gameData.awayid
+
+          innings = gameData.innings
+
+          // elaborate winning team
+          const homePoints = gameData.homeruns
+          const awayPoints = gameData.awayruns
+          if (homePoints === awayPoints) {
+            problems.push(`Game ended with a tie (${homePoints}:${awayPoints})`)
           }
-        })
-        gameScorer = scorers.join(', ')
-        console.log(gameScorer)
+          winner = homePoints > awayPoints ? 'home' : 'away'
 
-        const gameAnalysis: PBPGameAnalysis = {
-          variant,
-          homeTeamId,
-          awayTeamId,
-          innings,
-          winner,
-        }
+          // build game title
+          gameTitle = `#${gameData.gamenumber} - ${gameData.homeioc} ${homePoints} vs. ${awayPoints} ${gameData.awayioc} (${gameData.start})`
 
-        // analyze pitching
-        const pitchingProblems = analyzePitching(gameAnalysis, appData)
-        if (pitchingProblems.length > 0) {
-          problems.push(...pitchingProblems)
-        }
+          // get scorer(s) name
+          const scorers: string[] = []
+          gameData.assignments.forEach((a) => {
+            if (a.type === 1) { // 0 = Umpires, 1 = scorers, 2 = TCs
+              scorers.push(a.person?.label || 'UNKNOWN')
+            }
+          })
+          gameScorer = scorers.join(', ')
+          console.log(gameScorer)
 
-        gamePlays = appData.gamePlays.all
+          const gameAnalysis: PBPGameAnalysis = {
+            variant,
+            homeTeamId,
+            awayTeamId,
+            innings,
+            winner,
+          }
 
-        // analyze earned runs
-        const runProblems = await analyzeEarnedRuns(gamePlays)
-        if (runProblems.length > 0) {
-          problems.push(...runProblems)
-        }
+          // analyze pitching
+          const pitchingProblems = analyzePitching(gameAnalysis, appData)
+          if (pitchingProblems.length > 0) {
+            problems.push(...pitchingProblems)
+          }
 
-        // analyze batting / fielding // TODO refactor to separate check
+          gamePlays = appData.gamePlays.all
 
-        // cycle through plays
-        const inn = Object.keys(gamePlays).map(p => parseInt(p))
-        inn.forEach((key) => {
+          // analyze earned runs
+          const runProblems = await analyzeEarnedRuns(gamePlays)
+          if (runProblems.length > 0) {
+            problems.push(...runProblems)
+          }
+
+          // analyze batting / fielding // TODO refactor to separate check
+
+          // cycle through plays
+          const inn = Object.keys(gamePlays).map(p => parseInt(p))
+          inn.forEach((key) => {
           // top of inning
-          gamePlays[key]?.top?.forEach((_play) => {
+            gamePlays[key]?.top?.forEach((_play) => {
             // console.log(play.narrative)
             // TODO check hits + forced outs (singles/doubles/triples)
-          })
-          // bottom of inning
-          gamePlays[key]?.bot?.forEach((_play) => {
+            })
+            // bottom of inning
+            gamePlays[key]?.bot?.forEach((_play) => {
             // console.log(play.narrative)
             // TODO check hits + forced outs (singles/doubles/triples)
+            })
           })
-        })
+        }
+      } catch (err) {
+        console.error(err)
+        if (err instanceof Error) {
+          problems.push(err.message)
+        }
       }
-    } catch (err) {
-      console.error(err)
-      if (err instanceof Error) {
-        problems.push(err.message)
-      }
-    }
 
-    games.push({
-      link: gameLink,
-      game: gameTitle,
-      scorer: gameScorer,
-      result: problems.length === 0 ? 'OK' : 'ERR',
-      problems,
-    })
+      games.push({
+        link: gameLink,
+        game: gameTitle,
+        scorer: gameScorer,
+        result: problems.length === 0 ? 'OK' : 'ERR',
+        problems,
+      })
+    }
+  } finally {
+    if (browser) {
+      await browser.close()
+    }
   }
 
   // return JSON data
--- server/api/links.ts
+++ server/api/links.ts
@@ -5,46 +5,54 @@ export default defineEventHandler(async (event): Promise<string[]> => {
   const gameLinks: string[] = []
 
   const executablePath = import.meta.dev ? useRuntimeConfig().public.chromium as string : await chromium.executablePath()
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath,
-    headless: true,
-  })
 
-  const query = getQuery(event)
-
-  const variant = query.variant as string
-  const league = query.league as string
-
-  const month = query.month as string
-  const year = query.year as string || '2025'
-  const dateFrom = `${year}-${month || '01'}-01`
-  const dateTo = `${year}-${month || '12'}-31`
-
-  const targetCompetitions = [] as string[]
-  if (league) {
-    targetCompetitions.push(getLinkForLeague(league))
-  } else {
-    if (variant === 'softball') {
-      targetCompetitions.push(...LINKS_SOFTBALL)
-    } else {
-      targetCompetitions.push(...LINKS_BASEBALL)
-    }
-  }
-
-  for (const competition of targetCompetitions) {
-    console.log('Processing competition: ' + competition)
-    const leaguePage = await browser.newPage()
-    await leaguePage.goto(competition)
-    await leaguePage.waitForSelector('#app')
-
-    const rawData = await leaguePage.$eval('#app', el => el.getAttribute('data-page'))
-    const leagueData = JSON.parse(rawData).props as WBSCLeagueData
-    leagueData.games.forEach((game) => {
-      if (game.start_date >= dateFrom && game.start_date <= dateTo) {
-        gameLinks.push(`${competition}/box-score/${game.id}`)
-      }
+  let browser
+  try {
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath,
+      headless: true,
     })
+
+    const query = getQuery(event)
+
+    const variant = query.variant as string
+    const league = query.league as string
+
+    const month = query.month as string
+    const year = query.year as string || '2025'
+    const dateFrom = `${year}-${month || '01'}-01`
+    const dateTo = `${year}-${month || '12'}-31`
+
+    const targetCompetitions = [] as string[]
+    if (league) {
+      targetCompetitions.push(getLinkForLeague(league))
+    } else {
+      if (variant === 'softball') {
+        targetCompetitions.push(...LINKS_SOFTBALL)
+      } else {
+        targetCompetitions.push(...LINKS_BASEBALL)
+      }
+    }
+
+    for (const competition of targetCompetitions) {
+      console.log('Processing competition: ' + competition)
+      const leaguePage = await browser.newPage()
+      await leaguePage.goto(competition)
+      await leaguePage.waitForSelector('#app')
+
+      const rawData = await leaguePage.$eval('#app', el => el.getAttribute('data-page'))
+      const leagueData = JSON.parse(rawData).props as WBSCLeagueData
+      leagueData.games.forEach((game) => {
+        if (game.start_date >= dateFrom && game.start_date <= dateTo) {
+          gameLinks.push(`${competition}/box-score/${game.id}`)
+        }
+      })
+    }
+  } finally {
+    if (browser) {
+      await browser.close()
+    }
   }
 
   return gameLinks
